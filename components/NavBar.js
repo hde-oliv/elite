f8a81e5b18602bced979706bf1673b8535ea4787
hde-oliv <henriquerocha@tutanota.com>
2021-12-09 13:41:18 -0300

    feat: add new buttons on NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,23 +1,33 @@
-import { Box, Button, Flex, Input, useColorMode } from "@chakra-ui/react";
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Box, Button, Flex, IconButton, Input, useColorMode, Icon, Text, Image, Heading } from "@chakra-ui/react";
+import { MoonIcon, SunIcon, LinkIcon } from "@chakra-ui/icons";
+import MenuDrawer from "./MenuDrawer";
+import { SiDiscord, SiFacebook, SiGithub } from 'react-icons/si';
 
 export default function NavBar() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const links = {
+    discord: "/clown",
+    facebook: "/clown",
+    github: "/clown",
+  }
 
   return (
     <Box pl="5%" pr="5%" pt="1.5%" pb="1.5%">
       <Flex p="0%">
         <Flex flex="1" justifyContent="start" mr="auto">
-            <Button>Elite Fansub</Button>
+            <Heading>elite fansub</Heading>
         </Flex>
-        <Flex flex="5" justifyContent="center" alignSelf="center">
-          <Input placeholder="Search" />
+        <Flex flex="3" justifyContent="center" alignSelf="center">
+          <Input placeholder="Pesquisar" />
         </Flex>
         <Flex flex="1" justifyContent="end" ml="auto">
-          <Button onClick={() => toggleColorMode()} ml="auto" mr="auto">
+          <Button onClick={() => toggleColorMode()} mr="2%">
             {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
           </Button>
-            <Button>Animes</Button>
+          <IconButton onClick={() => window.open(links.discord, "_blank")} icon={<Icon as={SiDiscord} />}  mr="2%" />
+          <IconButton onClick={() => window.open(links.facebook, "_blank")} icon={<Icon as={SiFacebook} />}  mr="2%" />
+          <IconButton onClick={() => window.open(links.github, "_blank")} icon={<Icon as={SiGithub}/>}  mr="2%" />
+          <MenuDrawer />
         </Flex>
       </Flex>
     </Box>
