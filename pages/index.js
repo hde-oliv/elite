6f0b866c4f8aa04384f8b93ed4ee685a70c4fa00
hde-oliv <henriquerocha@tutanota.com>
2021-12-09 13:42:11 -0300

    feat: add navbar and button to home

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import NavBar from '../components/NavBar';
+import { Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-  <></>
+    <>
+      <NavBar pd="auto" />
+      <Button onClick={() => router.push('/animes/1')} >Tsugu Tsugumomo</Button>
+    </>
   );
 }
