e15b1cca400303afcacdba68df6dd99743dfc6fa
hde-oliv <henriquerocha@tutanota.com>
2021-12-09 13:41:28 -0300

    feat: create Clown page

---
+++ pages/clown.js
@@ -0,0 +1,12 @@
+import { Image, Center, Text, Flex } from "@chakra-ui/react";
+
+export default function Clown() {
+  return (
+    <Flex flexDirection="column">
+      <Image src={`./clown.svg`} height="90vh" mt="1%" />
+      <Center>
+        <Text as="b" mt="1%">Not implemented, go back!</Text>
+      </Center>
+    </Flex>
+  );
+}
