8a6ab11a629f951bfba4842334cf4497ce8fa65d
hde-oliv <henriquerocha@tutanota.com>
2021-12-09 17:36:01 -0300

    feat: add front page and dummy posts

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,65 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image, Box, Text, Button, Divider, Center, Heading } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import NavBar from "../components/NavBar";
 
-export default function Home() {
+export default function Home({ posts }) {
   const router = useRouter();
 
   return (
     <Flex flexDirection="column">
-      <NavBar pd="auto" />
-      <Flex>
-        <Flex justifyContent="start" mr="auto">
-          {/* Posts */}
+      <NavBar pd="auto"/>
+      <Flex width="100vw">
+        {/* Post Box Start */}
+        <Flex flex="4" ml="5%">
+          <Flex flexDirection="column" width="100%">
+            {posts.map((post) => (
+              <Flex p="5%" pt="2%" pb="3%" mb="4%" boxShadow="lg" rounded="md" borderRadius="md">
+                <Center flexDirection="column">
+                  <Image
+                    maxWidth="480px"
+                    src={post.image}
+                    alt={post.title}
+                    borderRadius="md"
+                  />
+                <Text mt="4%" as="i" color="grey">{post.author} - {post.date}</Text>
+                </Center>
+                <Flex pl="5%" flexDirection="column" maxWidth="100%" maxHeight="100%">
+                  <Heading textAlign="start" mb="5%">{post.title}</Heading>
+                  <Text noOfLines={[1, 2, 3, 4, 5]} maxHeight="100%">{post.text}</Text>
+                  <Flex justifyContent="end" justifySelf="end" mt="auto">
+                    <Button>
+                      <a href={`/post/${post.id}`}>Página do Anime</a>
+                    </Button>
+                    <Button>
+                      <a href={`/post/${post.id}`}>Leia Mais</a>
+                    </Button>
+                  </Flex>
+                </Flex>
+              </Flex>
+            ))}
+          </Flex>
         </Flex>
-        <Flex justifyContent="end" ml="auto">
-          {/* Sidebar */}
+        {/* Post Box End */}
+        {/* Sidebar Start */}
+        <Flex flex="1" justifyContent="center" mr="5%">
+          <Box>
+            {/* TODO: Add Sidebar */}
+          </Box>
         </Flex>
+        {/* Sidebar End */}
       </Flex>
     </Flex>
   );
 }
+
+export async function getServerSideProps({ params }) {
+    const request1 = await fetch(`http://localhost:3000/posts/mieruko-09.json`);
+    const mieruko8 = await request1.json();
+    const request2 = await fetch(`http://localhost:3000/posts/mieruko-08.json`)
+    const mieruko9 = await request2.json();
+
+    return {
+        props: { posts: [ mieruko8, mieruko9, mieruko8, mieruko9, mieruko8 ] },
+    }
+}
+
