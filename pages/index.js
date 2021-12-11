5d6dc5a17914f1644e966c5d60dbb85c2a138fa1
hde-oliv <henriquerocha@tutanota.com>
2021-12-10 22:12:43 -0300

    feat: add statusbar

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,47 @@
-import { Flex, Image, Box, Text, Button, Center, Heading } from "@chakra-ui/react";
+import {
+  Flex,
+  Image,
+  Box,
+  Text,
+  Button,
+  Center,
+  Heading,
+  Divider,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import NavBar from "../components/NavBar";
-import { ArrowForwardIcon } from "@chakra-ui/icons"
-import { getPosts} from "../lib/Firebase";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { getPosts } from "../lib/Firebase";
 
 export default function Home({ posts }) {
   const router = useRouter();
 
+  const status = {
+    name: "Mieruko",
+    status: "Aguardando lançamento",
+    next: "Ep. 10",
+  };
+
+  const statusList = [status, status, status];
+
   return (
     <Flex flexDirection="column">
-      <NavBar pd="auto"/>
-      <Flex width="99vw" pt="2%">
+      <NavBar pd="auto" />
+      <Flex pt="2%">
         {/* Post Box Start */}
         <Flex flex="4" ml="5%">
           <Flex flexDirection="column" width="100%">
             {posts.map((post, index) => (
-              <Flex key={index} p="4%" pt="2%" pb="3%" mb="2%" boxShadow="lg" rounded="md" borderRadius="md">
+              <Flex
+                key={index}
+                p="4%"
+                pt="2%"
+                pb="3%"
+                mb="2%"
+                boxShadow="lg"
+                rounded="md"
+                borderRadius="md"
+              >
                 <Center flexDirection="column">
                   <Image
                     maxWidth="480px"
@@ -24,16 +50,37 @@ export default function Home({ posts }) {
                     alt={post.title}
                     borderRadius="md"
                   />
-                <Text mt="4%" as="i" color="grey">{post.author} - {post.date}</Text>
+                  <Text mt="4%" as="i" color="grey">
+                    {post.author} - {post.date}
+                  </Text>
                 </Center>
-                <Flex pl="5%" flexDirection="column" maxWidth="100%" maxHeight="100%">
-                  <Heading textAlign="start" mb="5%">{post.title}</Heading>
-                  <Text noOfLines={[1, 2, 3, 4, 5]} maxHeight="100%">{post.text}</Text>
+                <Flex
+                  pl="5%"
+                  flexDirection="column"
+                  maxWidth="100%"
+                  maxHeight="100%"
+                >
+                  <Heading textAlign="start" mb="5%">
+                    {post.title}
+                  </Heading>
+                  <Text noOfLines={[1, 2, 3, 4, 5]} maxHeight="100%">
+                    {post.text}
+                  </Text>
                   <Flex justifyContent="end" justifySelf="end" mt="auto">
-                    <Button mr="2%" variant="outline" onClick={() => router.push(`/posts/${post.date}/${post.title}`)}>
+                    <Button
+                      mr="2%"
+                      variant="outline"
+                      onClick={() =>
+                        router.push(`/posts/${post.date}/${post.title}`)
+                      }
+                    >
                       Leia Mais
                     </Button>
-                    <Button rightIcon={<ArrowForwardIcon />} colorScheme='blue' onClick={() => router.push(`/animes/${post.anime}`)}>
+                    <Button
+                      rightIcon={<ArrowForwardIcon />}
+                      colorScheme="blue"
+                      onClick={() => router.push(`/animes/${post.anime}`)}
+                    >
                       Página do Anime
                     </Button>
                   </Flex>
@@ -44,10 +91,31 @@ export default function Home({ posts }) {
         </Flex>
         {/* Post Box End */}
         {/* Sidebar Start */}
-        <Flex flex="1" justifyContent="center" mr="5%">
-          <Box>
-            {/* TODO: Add Sidebar */}
-          </Box>
+        <Flex flex="1" justifyContent="end" pl="5%" pr="5%">
+          <Center flexDirection="column" justifyContent="start" width="100%">
+            {/* Project Status Start */}
+            <Box
+              boxShadow="lg"
+              rounded="md"
+              borderRadius="md"
+              ml="1%"
+              width="100%"
+              textAlign="center"
+              p="6%"
+            >
+              <Heading size="md">Status dos Projetos</Heading>
+              <Divider size="2px" pt="4%" />
+              {statusList.map((status, index) => (
+                <Box key={index} textAlign="left" pl="2%" pt="6%">
+                  <Heading size="sm">• {status.name}</Heading>
+                  <Text as="i" color="grey">
+                    {status.next} - {status.status}
+                  </Text>
+                </Box>
+              ))}
+            </Box>
+            {/* Project Status End */}
+          </Center>
         </Flex>
         {/* Sidebar End */}
       </Flex>
@@ -57,8 +125,7 @@ export default function Home({ posts }) {
 
 export async function getServerSideProps() {
   const postList = await getPosts();
-    return {
-        props: { posts: postList },
-    }
+  return {
+    props: { posts: postList },
+  };
 }
-
