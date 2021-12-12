f3eb00f481fdeb487a5ea1192344cb43e7d9201a
hde-oliv <henriquerocha@tutanota.com>
2021-12-12 14:27:50 -0300

    feat: add specials section

---
+++ pages/specials/[id].js
@@ -0,0 +1,106 @@
+import {
+  Flex,
+  Text,
+  Box,
+  Image,
+  Center,
+  VStack,
+  Grid,
+  Button,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import NavBar from "../../components/NavBar";
+import { getSpecial } from "../../lib/Firebase";
+
+export default function SpecialPage({ special }) {
+  return (
+    <Flex flexDirection="column" height="100vh">
+      <NavBar pd="auto" justifyContent="start" />
+      <Box
+        p="5%"
+        pt="2%"
+        pb="auto"
+        justifyContent="end"
+        border="px"
+        borderColor="gray.100"
+      >
+        <Box>
+          <Flex boxShadow="lg" p="6" rounded="md" borderRadius="md">
+            {/* Image Box Start */}
+            <Center flex="1">
+              <Flex p="4%" pl="0%">
+                <Link href={special.mal_url} passHref>
+                  <Image
+                    src={special.image}
+                    cursor="pointer"
+                    maxHeight="600px"
+                    maxWidth="439px"
+                    alt={special.title}
+                  />
+                </Link>
+              </Flex>
+            </Center>
+            {/* Image Box End */}
+            {/* Text Box Start */}
+            <Box flex="2" pt="1%">
+              <VStack spacing="4%">
+                <Text fontSize="5xl" as="b">
+                  {special.title}
+                </Text>
+                <Text fontSize="md" textAlign="center">
+                  {special.description}
+                </Text>
+                <Box>
+                  {special.links.length !== 1 ? (
+                    <Grid templateColumns="repeat(12, 1fr)" gap={6}>
+                      {special.links.map((link, index) => (
+                        <Button
+                          key={index}
+                          onClick={() => window.open(link, "_blank")}
+                        >
+                          {index + 1}
+                        </Button>
+                      ))}
+                    </Grid>
+                  ) : (
+                    <Button
+                      onClick={() => window.open(special.links[0], "_blank")}
+                    >
+                      Link
+                    </Button>
+                  )}
+                </Box>
+                <Box>
+                  {special.torrents.length > 0 ? (
+                    <Flex flexDirection="row">
+                      {special.torrents.map((torrent, index) => (
+                        <Button
+                          key={index}
+                          onClick={() => window.open(torrent, "_blank")}
+                          mr="4%"
+                        >
+                          Torrent
+                        </Button>
+                      ))}
+                    </Flex>
+                  ) : (
+                    <></>
+                  )}
+                </Box>
+              </VStack>
+            </Box>
+          </Flex>
+        </Box>
+        {/* Text Box End */}
+      </Box>
+    </Flex>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const specialData = await getSpecial(params.id);
+
+  return {
+    props: { special: specialData },
+  };
+}
---
+++ pages/specials/index.js
@@ -0,0 +1,55 @@
+import {Box, Button, Center, Divider, Flex, Grid, Heading, Image, Text} from "@chakra-ui/react";
+import NavBar from "../../components/NavBar";
+import {ArrowForwardIcon} from "@chakra-ui/icons";
+import {getSpecials} from "../../lib/Firebase";
+import {useRouter} from "next/router";
+
+export default function Specials({ specialList }) {
+  const router = useRouter();
+
+  return (
+    <Flex flexDirection="column" height="100vh">
+      <NavBar pd="auto" justifyContent="start" />
+      <Box
+        p="5%"
+        pt="2%"
+        pb="auto"
+        justifyContent="end"
+        border="px"
+        borderColor="gray.100"
+      >
+        <Box>
+          <Grid templateColumns="repeat(3, 1fr)" columnGap="5%" rowGap="2%">
+            {specialList.map((special, index) => (
+              <Flex key={index} boxShadow="lg" p="6" rounded="md" borderRadius="md" mb="3%">
+                <Flex pr="3%" pl="3%" width="100%">
+                  <Image alt={special.title} src={special.image} maxHeight="250px" />
+                  <Box width="100%" alignSelf="center" textAlign="center" pl="8%">
+                    <Heading fontSize="3xl">{special.title}</Heading>
+                    <Button
+                      size="sm"
+                      rightIcon={<ArrowForwardIcon />}
+                      colorScheme="blue"
+                      mt="5%"
+                      onClick={() => router.push(`/specials/${special.slug}`)}
+                    >
+                      Página
+                    </Button>
+                  </Box>
+                </Flex>
+              </Flex>
+            ))}
+          </Grid>
+        </Box>
+      </Box>
+    </Flex>
+  );
+}
+
+export async function getServerSideProps() {
+  const specialsData = await getSpecials();
+
+  return {
+    props: { specialList: specialsData },
+  };
+}
